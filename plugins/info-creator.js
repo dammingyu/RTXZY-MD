ad39d1d2bbed6fc6d366e585e78c23048df3d99e
dammingyu <114360202+dammingyu@users.noreply.github.com>
2022-11-15 04:21:16 +0900

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -10,14 +10,14 @@ VERSION:3.0
 N:Sy;Bot;;;
 FN: TioXd
 item.ORG: Creator Bot
-item1.TEL;waid=6282221792667:6282221792667@s.whatsapp.net
+item1.TEL;waid=6289508996342:6289508996342@s.whatsapp.net
 item1.X-ABLabel:Nomor Creator Bot / Owner Bot
-item2.EMAIL;type=INTERNET:Kalo Chat Gak Penting Gak Akan Di Balas.
+item2.EMAIL;type=INTERNET:DONT SPAMMING OR CHAT WITHOUT PERMISSION
 item2.X-ABLabel:Email
 item3.ADR:;;🇮🇩INDONESIA;;;;
 item3.X-ABADR:ac
-item3.X-ABLabel:📍Lokasi
-item4.EMAIL;type=INTERNET:Menerima kritik & saran user kok :)
+item3.X-ABLabel:CENTRAL JAVA
+item4.EMAIL;type=INTERNET:QUESTION FOR ADMIN
 item4.X-ABLabel:Contact 👤
 item5.URL:${instagram}
 item5.X-ABLabel:Website
@@ -26,7 +26,7 @@ const sentMsg  = await conn.sendMessage(
     m.chat,
     { 
         contacts: { 
-            displayName: 'OWNER GW', 
+            displayName: 'OWNER MINGYU', 
             contacts: [{ vcard }] 
         }
     }
